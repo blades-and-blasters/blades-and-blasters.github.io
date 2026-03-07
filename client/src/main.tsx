b9377811cbcfa939bda174739c04d59d46542aac
A_Grady <adrianjgrady@gmail.com>
2026-03-07 03:22:31 -0600

    Adjusted home page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+if (sessionStorage.redirect ) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.location.href = redirect;
+}
 createRoot(document.getElementById("root")!).render(<App />);
