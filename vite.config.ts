43387870413b160fa7e612ae4fac683bb91caeeb
A_Grady <adrianjgrady@gmail.com>
2026-03-07 12:05:47 -0600

    changing websites once again

--- vite.config.ts
+++ vite.config.ts
@@ -166,7 +166,7 @@ export default defineConfig({
   },
   envDir: path.resolve(import.meta.dirname),
   root: path.resolve(import.meta.dirname, "client"),
-  base: '/home/',
+  base: '/',
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
